f354883974205cc560cce62b0d8508203286a824
arnorhs <arnorhs@gmail.com>
2012-01-01 11:06:54 -0800

    removed everyauth, added passport

--- app.js
+++ app.js
@@ -6,13 +6,41 @@
 var express = require('express')
   , stylus = require('stylus')
   , routes = require('./routes')
-  , everyauth = require('everyauth')
+  , passport = require('passport')
+  , LocalAuthStrategy = require('passport-local').Strategy
   , bragtimer = require('./bragtimer')
 
 var app = module.exports = express.createServer();
 
-// Configuration
+// Passport auth
+passport.use(new LocalAuthStrategy(
+    function(username, password, done) {
+        console.log('checking user');
+        done(null,{username: username, password:password, id:2343});
+        /*
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) { return done(null, false); }
+            if (!user.validPassword(password)) { return done(null, false); }
+            return done(null, user);
+        });
+        */
+    }
+));
+passport.serializeUser(function(user, done) {
+    console.log('serialize user',user);
 
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    console.log('deserialize user');
+    User.findOne(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+// Configuration
 app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
@@ -20,15 +48,17 @@ app.configure(function(){
     app.use(express.methodOverride());
     app.use(express.cookieParser());
     app.use(express.session({ secret: 'your secret here' }));
+    // passport config
+    app.use(passport.initialize());
+    app.use(passport.session());
+    // stylus my baby
     app.use(stylus.middleware({ src: __dirname + '/public' }));
-    app.use(everyauth.middleware());
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-  everyauth.debug = true;
 });
 
 app.configure('production', function(){
@@ -36,12 +66,11 @@ app.configure('production', function(){
 });
 
 // every auth routes and helpers
-bragtimer.setupEveryAuth(everyauth);
-everyauth.helpExpress(app);
 
 // Routes
-
 app.get('/', routes.index);
+app.get('/login', routes.login);
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Brag Timer' })
 };
+
+exports.login = function(req, res){
+  res.render('login', { title: 'Login', layout: 'static-layout' })
+};
